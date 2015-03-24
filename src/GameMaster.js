c567752e5e717c305974d898592c2eb41f62cec9
joshuastray <joshua.astray.14@gmail.com>
2015-03-25 00:51:22 +0800

    warning ui

--- src/GameMaster.js
+++ src/GameMaster.js
@@ -153,7 +153,6 @@
 
         showPiglet:function(isFront){
             var piglet = cc.pool.getFromPool(Piglet);
-            cc.log("show piglet" + piglet);
             piglet.reset();
             var appearPos = this.getFocusPointOfHeros();
             var randomX = Math.random()*0.2+1;
@@ -248,75 +247,77 @@
             cc.log("************************************************************ stage: "+stage)
             if(stage === 1){
                 if(MonsterManager.length < EXIST_MIN_MONSTER){
+
+            this.showDialog();
                     for(let i = 0; i < 4; ++i)
                         this.randomshowMonster(true);
-
-                    stage = 2;
-                }
-            }else if(stage === 2){
-                if(MonsterManager.length < EXIST_MIN_MONSTER){
-                    for(let i = 0; i < 4; ++i)
-                        this.randomshowMonster(true);
-
-                    stage = 3;
-                }
-            }else if(stage === 3){
-                if(MonsterManager.length == 0){
-                    for(let i = 0; i < HeroManager.length; ++i){
-                        HeroManager[i]._goRight = true;
-                    }
-                    stage = 4;
-                }
-            }else if(stage === 4){
-                if(this.getFocusPointOfHeros().x > battleSiteX[1]){
-                    for(let i = 0; i < 3; ++i)
-                        this.randomshowMonster(true);
-                    for(let i = 0; i < 4; ++i)
-                        this.randomshowMonster(false)
-
-                    stage = 5;
-                }
-            }else if(stage == 5){
-                if(MonsterManager.length < EXIST_MIN_MONSTER){
-                    for(let i = 0; i < 4; ++i)
-                        this.randomshowMonster(true);
-
-                    stage = 6;
-                }
-            }else if(stage === 6){
-                if(MonsterManager.length < EXIST_MIN_MONSTER){
-                    for(let i = 0; i < 4; ++i)
-                        this.randomshowMonster(false);
-
-                    stage = 7;
-                }
-            }else if(stage === 7){
-                if(MonsterManager.length == 0){
-                    for(let i = 0; i < HeroManager.length; ++i){
-                        let hero = HeroManager[i];
-                        if(hero)
-                            hero._goRight = true;
-                    }
-
-
-                    var d = cc.pool.getFromPool(Dragon);
-                    if(d)
-                        d.removeFromParent();
-                    
-                    for(let i = 0; i < monsterCount.piglet; ++i){
-                        var p = cc.pool.getFromPool(Piglet);
-                        if(p)
-                            p.removeFromParent();
-                    }
-
-                    for(let i = 0; i < monsterCount.slime; ++i){
-                        var s = cc.pool.getFromPool(Slime);
-                        if(s)
-                            s.removeFromParent();
-                    }
 
                     stage = 8;
                 }
+            // }else if(stage === 2){
+            //     if(MonsterManager.length < EXIST_MIN_MONSTER){
+            //         for(let i = 0; i < 4; ++i)
+            //             this.randomshowMonster(true);
+
+            //         stage = 3;
+            //     }
+            // }else if(stage === 3){
+            //     if(MonsterManager.length == 0){
+            //         for(let i = 0; i < HeroManager.length; ++i){
+            //             HeroManager[i]._goRight = true;
+            //         }
+            //         stage = 4;
+            //     }
+            // }else if(stage === 4){
+            //     if(this.getFocusPointOfHeros().x > battleSiteX[1]){
+            //         for(let i = 0; i < 3; ++i)
+            //             this.randomshowMonster(true);
+            //         for(let i = 0; i < 4; ++i)
+            //             this.randomshowMonster(false)
+
+            //         stage = 5;
+            //     }
+            // }else if(stage == 5){
+            //     if(MonsterManager.length < EXIST_MIN_MONSTER){
+            //         for(let i = 0; i < 4; ++i)
+            //             this.randomshowMonster(true);
+
+            //         stage = 6;
+            //     }
+            // }else if(stage === 6){
+            //     if(MonsterManager.length < EXIST_MIN_MONSTER){
+            //         for(let i = 0; i < 4; ++i)
+            //             this.randomshowMonster(false);
+
+            //         stage = 7;
+            //     }
+            // }else if(stage === 7){
+            //     if(MonsterManager.length == 0){
+            //         for(let i = 0; i < HeroManager.length; ++i){
+            //             let hero = HeroManager[i];
+            //             if(hero)
+            //                 hero._goRight = true;
+            //         }
+
+
+            //         var d = cc.pool.getFromPool(Dragon);
+            //         if(d)
+            //             d.removeFromParent();
+                    
+            //         for(let i = 0; i < monsterCount.piglet; ++i){
+            //             var p = cc.pool.getFromPool(Piglet);
+            //             if(p)
+            //                 p.removeFromParent();
+            //         }
+
+            //         for(let i = 0; i < monsterCount.slime; ++i){
+            //             var s = cc.pool.getFromPool(Slime);
+            //             if(s)
+            //                 s.removeFromParent();
+            //         }
+
+            //         stage = 8;
+            //     }
             }else if(stage === 8){
                 if(this.getFocusPointOfHeros().x > battleSiteX[2]){
                     this.showWarning();
@@ -327,6 +328,115 @@
         },
 
         showWarning:function(){
+            cc.Texture2D.setDefaultAlphaPixelFormat(0);//AUTO
+            var waring = new cc.Layer();
+            var waring_logo = new cc.Sprite("#caution.png");
+            cc.Texture2D.setDefaultAlphaPixelFormat(4);//RGB565
+            waring_logo.setPosition(cc.winSize.width*0.5, cc.winSize.height*0.5);
+            waring_logo.setVertexZ(1);
+            waring_logo.setGlobalZOrder(UIZorder);
+
+            var self = this;
+            function showdialog(){
+                waring.removeFromParent();
+                self.showDialog();
+                cc.audioEngine.playEffect("audios/effects/boss/boss.mp3");
+            }
+
+            waring_logo.runAction(cc.sequence(
+                    cc.delayTime(0.5),
+                    cc.blink(1.5, 3).easing(cc.easeSineOut()),
+                    cc.callFunc(showdialog)
+                ));
+            waring.addChild(waring_logo);
+            waring.setScale(0.5);
+            waring.setVertexZ(-cc.director.getZEye()/2);
+            waring.ignoreAnchorPointForPosition(false);
+            waring.setLocalZOrder(999);
+            this._layer._camera.addChild(waring);
+        },
+
+        showDialog:function(){
+            cc.Texture2D.setDefaultAlphaPixelFormat(0);//AUTO
+            var colorLayer = new cc.LayerColor(cc.color(10, 10, 10, 150));
+            colorLayer.ignoreAnchorPointForPosition(false);
+            colorLayer.setVertexZ(-cc.director.getZEye()/5);
+            colorLayer.setGlobalZOrder(0);
+            this._layer._camera.addChild(colorLayer);
+
+            //create dialog
+            var dialog = new cc.Layer();
+            dialog.x = -cc.winSize.width*0.025;
+
+            var outframe = new cc.Sprite("#outframe.png");
+            outframe.setPosition(cc.winSize.width*0.55, cc.winSize.height*0.27);
+            outframe.setScale(0.6*resolutionRate);
+            outframe.setGlobalZOrder(UIZorder+1);
+            dialog.addChild(outframe);
+
+            var inframe = new cc.Sprite("#inframe.png");
+            inframe.setPosition(cc.winSize.width*0.67, cc.winSize.height*0.27);
+            inframe.setScale(0.5*resolutionRate);
+            inframe.setGlobalZOrder(UIZorder);
+            dialog.addChild(inframe);
+
+            var bossicon = new cc.Sprite("#bossicon.png");
+            bossicon.setPosition(cc.winSize.width*0.42, cc.winSize.height*0.46);
+            bossicon.setScale(0.75*resolutionRate);
+            bossicon.setFlippedX(true);
+            bossicon.setGlobalZOrder(UIZorder);
+            dialog.addChild(bossicon);
+
+            var bosslogo = new cc.Sprite("#bosslogo.png");
+            bosslogo.setPosition(cc.winSize.width*0.417, cc.winSize.height*0.265);
+            bosslogo.setScale(0.74*resolutionRate);
+            dialog.addChild(bosslogo);
+
+            var ttfconf = {outlineSize:0, fontSize:24, glyphs:0, fontFilePath:"fonts/britanic bold.ttf"};
+            var text = cc.Label.createWithTTF(ttfconf, "How dare you???");
+            text.setPosition(cc.winSize.width*0.68, cc.winSize.height*0.27);
+            text.setGlobalZOrder(UIZorder+1);
+            dialog.addChild(text);
+
+            // dialog.setScale
+            dialog.setScale(0.1);
+            dialog.ignoreAnchorPointForPosition(false);
+            dialog.setVertexZ(-cc.director.getZEye()/3);
+            dialog.setGlobalZOrder(UIZorder);
+            this._layer._camera.addChild(dialog);
+
+            function pausegame(){
+                for(let i = 0; i < HeroManager.length; ++i){
+                    HeroManager[i].idleMode();
+                    HeroManager[i].setAIEnabled(false);
+                }
+            }
+            dialog.runAction(cc.sequence(
+                cc.scaleTo(0.5, 0.5), 
+                cc.callFunc(pausegame)
+                ))
+            this._layer._uiLayer.setVisible(false);
+
+            var self = this;
+            function removeDialog(){
+                dialog.removeFromParent();
+                colorLayer.removeFromParent();
+                self._layer._uiLayer.setVisible(true);
+                for(let i = 0; i < HeroManager.length; ++i){
+                    HeroManager[i].setAIEnabled(true);
+                }
+                self.showBoss();
+            }
+            dialog.runAction(cc.sequence(
+                cc.delayTime(3),
+                cc.callFunc(removeDialog)
+                ));
+            //todo
+
+            cc.Texture2D.setDefaultAlphaPixelFormat(4);//RGB565
+        },
+
+        showBoss:function(){
 
         },
 
